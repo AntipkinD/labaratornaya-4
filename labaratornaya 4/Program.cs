b59b898d843d92e1f322bafba2f702dbbcebf3f5
mushroomvoodoo <normeeboy@gmail.com>
2023-01-30 08:16:55 +0300

    создал возможность ввода количества пар

--- labaratornaya 4/Program.cs
+++ labaratornaya 4/Program.cs
@@ -5,5 +5,7 @@
         int maxLength = 1000;
         var counter = new int[maxLength];
 
+        for (int N = int.Parse(Console.ReadLine()); N > 0; N--)
+            counter[int.Parse(Console.ReadLine())]++;
     }
 }
