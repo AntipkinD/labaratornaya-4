6238781ccd5ea5368c2d612f20e79d41d31606fd
mushroomvoodoo <normeeboy@gmail.com>
2023-02-03 11:59:36 +0300

    .

--- labaratornaya 4/Program.cs
+++ labaratornaya 4/Program.cs
@@ -2,15 +2,15 @@
 
 internal class Program
 {
-    int maxLength = 1000;
-    private static void Main(string[] args)
+    const int maxLength = 10000;
+    static void Main(string[] args)
     {
         var counter = new int[maxLength];
 
         for (int i = Convert.ToInt32(Console.ReadLine()); i > 0; i--)
             counter[Convert.ToInt32(Console.ReadLine())]++;
 
-        for (int j = 0; j < counter.Length; j++)
+        for (int j = 0; j < 3; j++)
         {
             if (counter[j] != 0)
                 Console.WriteLine($"{j} {counter[j]}");
